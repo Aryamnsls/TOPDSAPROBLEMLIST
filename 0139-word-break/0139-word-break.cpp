b6181c120ea1888c40c804f07967f08caa481ce6
Aryaman M Singha <91653939+Aryamnsls@users.noreply.github.com>
2023-08-05 00:22:07 +0530

    Time: 14 ms (50.55%), Space: 13.4 MB (35.7%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+
+    bool wordBreak(string s, vector<string>& wordDict) {
+        
+        // Implementation using of the bfs 
+
+         unordered_set<string> words(wordDict.begin(), wordDict.end());
+        queue<int> queue;
+        vector<bool> seen(s.length(), false);
+        queue.push(0);
+        
+        while (!queue.empty()) {
+            int start = queue.front();
+            queue.pop();
+            
+            if (start == s.length()) {
+                return true;
+            }
+            
+            for (int end = start + 1; end <= s.length(); end++) {
+                if (seen[end]) {
+                    continue;
+                }
+             
+                             if (words.find(s.substr(start, end - start)) != words.end()) {
+                    queue.push(end);
+                    seen[end] = true;
+                }
+            }
+        }
+        return false;
+    }
+};
