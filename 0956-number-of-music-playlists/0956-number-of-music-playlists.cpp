bfa222f5c478e5e82a3d5575014b1efac115c631
Aryaman M Singha <91653939+Aryamnsls@users.noreply.github.com>
2023-08-06 23:44:36 +0530

    Time: 6 ms (49.1%), Space: 9.3 MB (17.12%) - LeetHub

--- 0956-number-of-music-playlists/0956-number-of-music-playlists.cpp
+++ 0956-number-of-music-playlists/0956-number-of-music-playlists.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
     int numMusicPlaylists(int n, int goal, int k) {
-        
-        const int MOD = 1e9 +7;
+        // Using of the recursion
 
-        vector<vector<long long>> dp(2, vector<long long>(n+1, 0));
+        const int MOD = 1e9+7;
+
+        vector<vector<long long>> 
+        dp(goal +1, vector<long long>(n+1, 0));
+
         dp[0][0] = 1;
 
+        for(int i = 1; i<=goal; i++){
 
-        for(int i = 1;i<=goal; i++){
+            for(int j = 1; j<=n; j++){
 
-            dp[i%2][0] = 0;
+                // Choose a new song from the remaining 
+                // (n - j + 1)songs
 
-            for(int j = 1; j<=min(i,n); j++){
-                
-                      dp[i%2][j] = dp[(i-1)%2][j-1] * (n-(j-1))% MOD;
+                dp[i][j] += dp[i-1][j-1] *(n-j+1);
 
-                      if(j>k)
+                dp[i][j] %= MOD;
 
-                      dp[i%2][j] = (dp[i%2][j] + dp[(i-1)%2][j] * 
-                      (j-k)) % MOD;
-                  }
+                if(j>k){
+
+                    // Choose a song from the already played songs, considering k different songs
+                    dp[i][j] += dp[i-1][j] * (j-k);
+                    dp[i][j] %= MOD;
+                }
             }
-
-            return dp[goal%2][n];
         }
-    
+        return dp[goal][n];
+    }
 };
