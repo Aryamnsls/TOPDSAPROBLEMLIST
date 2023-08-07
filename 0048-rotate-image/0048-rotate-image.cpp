5df934e30b4254b8f400fa33a544c330b30c3307
Aryaman M Singha <91653939+Aryamnsls@users.noreply.github.com>
2023-08-07 17:31:04 +0530

    Time: 4 ms (34.34%), Space: 7.1 MB (45.6%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -3,22 +3,53 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         
         // Grid form approach 
-        // Dp - Inclusive form 
+       
           
-          int size  = matrix.size();
-        // Dedicated for the row - wise 
+        //   int size  = matrix.size();
+        // // Dedicated for the row - wise 
 
-        for(int i = 0;i<size; i++)
-        // Dedicated for the column wise
+        // for(int i = 0;i<size; i++)
+        // // Dedicated for the column wise
 
-        for(int j = 0; j<i; j++)
+        // for(int j = 0; j<i; j++)
 
-            // swap the matrix 
+        //     // swap the matrix 
 
-            swap(matrix[i][j], matrix[j][i]);
+        //     swap(matrix[i][j], matrix[j][i]);
 
-            for(int i = 0; i<size; i++)
-                reverse(matrix[i].begin(), matrix[i].end());
+        //     for(int i = 0; i<size; i++)
+
+        //     // Sort the number 
+            
+        //         reverse(matrix[i].begin(), matrix[i].end());
+
+
+        // Another Approach 
+
+        int size = matrix.size();
+
+        // Dedicated for the row-wise 
+
+        for(int i = 0; i<size; i++)
+
+            // Dedicated for the column-wise 
+
+            for(int j = 0; j<i; j++)
+
+                // Swap the matrix 
+
+                swap(matrix[i][j], matrix[j][i]);
+
+                // Traverse the number till the size 
+
+                for(int i = 0; i<size; i++)
+
+                    // Rverse the Number 
+
+                    reverse(matrix[i]. begin(), matrix[i].end());
+                
+            
+        
             
 
     }
