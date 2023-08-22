5174d1a8961db0deb2ef4f3d72204cc5777f1832
Aryaman M Singha <91653939+Aryamnsls@users.noreply.github.com>
2023-08-22 16:03:04 +0530

    Time: 6 ms (59.12%), Space: 8 MB (45.6%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        // Brute- Force 
+    vector<int>ans(n+1);
+        for (int i = 0; i <= n; ++i) {
+        int num = i;
+        int count = 0;
+
+        while (num > 0) {
+            count += num & 1;  // Check if the last bit is set
+            num >>= 1;        // Right shift to remove the last bit
+        }
+
+        ans[i] = count;
+    }
+     return ans;
+    }
+};
