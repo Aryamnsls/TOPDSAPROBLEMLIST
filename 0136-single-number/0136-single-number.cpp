eccad01b533fca9d9c3f3860836dc5c6621b17ba
Aryaman M Singha <91653939+Aryamnsls@users.noreply.github.com>
2023-08-22 15:12:22 +0530

    Time: 13 ms (83.94%), Space: 17 MB (31.37%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,14 +1,27 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-      sort(nums.begin(), nums.end());
-      for(int i = 1; i<nums.size(); i+=2)
-      {
-          if(nums[i]!=nums[i-1])
-              return nums[i-1];
-          
-      }
+        // Brute- Force 
 
-      return nums[nums.size()-1];
+    //   sort(nums.begin(), nums.end());
+    //   for(int i = 1; i<nums.size(); i+=2)
+    //   {
+    //       if(nums[i]!=nums[i-1])
+    //           return nums[i-1];
+          
+    //   }
+
+    //   return nums[nums.size()-1];
+
+    // Optimised Approach using of the XOR Operator 
+
+    int ans = 0;
+    for(int i=  0; i<nums.size(); i++)
+    {
+        // Using of the XOR Operationn
+        ans ^= nums[i];
+    }
+
+    return ans;
     }
 };
