8f314320b9ee6b4eeb3ed6c0fbe0083fbb7c8d92
Aryaman M Singha <91653939+Aryamnsls@users.noreply.github.com>
2023-08-28 19:40:20 +0530

    Time: 3 ms (39.45%), Space: 6.1 MB (28.43%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-        if (n == 0)
-            return 1.0;
-
-        long long absN = n;  // Use 64-bit long long for absN
-        if (n < 0) {
-            x = 1 / x;
-            absN = -absN; // Convert to positive using long long
+    double myPow(double x, long long  n) {
+        if(n<0)
+        {
+            x = 1/x;
+            n = -n;
         }
 
-        double result = 1.0;
-        while (absN > 0) {
-            // If the least significant bit of absN is 1, multiply the result by x
-            if (absN & 1)
-                result *= x;
+        double result = 1;
+        double current_product = x;
 
-            // Square x and halve absN
-            x *= x;
-            absN >>= 1;
+
+        while(n>0)
+        {
+              if(n%2 == 1)
+              {
+                  result = result * current_product;
+              }
+              current_product = current_product * current_product;
+              n = n/2;
         }
 
         return result;
