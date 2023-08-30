c1012a16260adc609fd4480d2b9550299d1bac4e
Aryaman M Singha <91653939+Aryamnsls@users.noreply.github.com>
2023-08-30 11:55:28 +0530

    Time: 81 ms (95.5%), Space: 54.7 MB (42.04%) - LeetHub

---
+++ 2450-minimum-replacements-to-sort-the-array/2450-minimum-replacements-to-sort-the-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    long long minimumReplacement(vector<int>& nums) {
+        
+        // Used to sort the array 
+
+        // sort(nums.begin(), nums.end());
+        
+        // Size of the nums 
+
+        int n = nums.size();
+        int last = nums[n-1];
+        long long ans = 0;  // Used to store the value 
+
+        for(int i = n-2; i>=0; i--){
+            if(nums[i] > last){
+                int t = nums[i]/ last;
+                if(nums[i] % last) t++;
+                last= nums[i]/t;
+                ans+=t-1;
+
+            }else{
+                last = nums[i];
+            }
+        }
+        return ans;
+    }
+};
