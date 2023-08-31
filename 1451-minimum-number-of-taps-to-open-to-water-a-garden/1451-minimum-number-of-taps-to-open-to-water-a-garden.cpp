e409e44ed964692986a1c085f1001f3d6148ba86
Aryaman M Singha <91653939+Aryamnsls@users.noreply.github.com>
2023-08-31 15:29:19 +0530

    Time: 30 ms (62.33%), Space: 14.6 MB (55.15%) - LeetHub

--- 1451-minimum-number-of-taps-to-open-to-water-a-garden/1451-minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1451-minimum-number-of-taps-to-open-to-water-a-garden/1451-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -3,29 +3,49 @@ public:
     int minTaps(int n, vector<int>& ranges) {
         // Greedy Approach 
 
-        vector<int>arr(n+1, 0);
-        for(int i = 0; i<ranges.size(); i++)
-        {
-            if(ranges[i]==0)
-            continue;
+        // vector<int>arr(n+1, 0);
+        // for(int i = 0; i<ranges.size(); i++)
+        // {
+        //     if(ranges[i]==0)
+        //     continue;
 
-            int left = max(0, i-ranges[i]);
-            arr[left] = max(arr[left], i+ranges[i]);
-        }
+        //     int left = max(0, i-ranges[i]);
+        //     arr[left] = max(arr[left], i+ranges[i]);
+        // }
 
-        int end = 0, far_end_ranges = 0, cnt =  0;
-        for(int i = 0; i<=n; i++)
-        {
-            if(i>end)
-            {
-                if(far_end_ranges<=end) return-1;
-                end = far_end_ranges;
-                cnt++;
-            }
-            far_end_ranges = max(far_end_ranges, arr[i]);
+        // int end = 0, far_end_ranges = 0, cnt =  0;
+        // for(int i = 0; i<=n; i++)
+        // {
+        //     if(i>end)
+        //     {
+        //         if(far_end_ranges<=end) return-1;
+        //         end = far_end_ranges;
+        //         cnt++;
+        //     }
+        //     far_end_ranges = max(far_end_ranges, arr[i]);
 
-        }
+        // }
 
-        return cnt + (end<n);
+        // return cnt + (end<n);
+
+        // optimised approach Using of the Top-Bottom Approach 
+
+          vector<int>dp(n+1, n+1);  // Create a vector 
+
+          dp[0] = 0;
+
+          for(int i =  0; i<=n; i+=1)
+          {
+ int left = max(0, i-ranges[i]), right = min(i+ranges[i], n);
+
+         for(int j = left; j<=right; j+=1)
+         {
+             dp[j] = min(dp[j], dp[left]+1);
+         }
+
+
+          }
+
+          return dp[n] == n+1? -1 : dp[n];
     }
 };
