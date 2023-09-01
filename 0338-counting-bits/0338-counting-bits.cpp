313e16c8ad1a29f0939a1a96bb7da7478cfc16e1
Aryaman M Singha <91653939+Aryamnsls@users.noreply.github.com>
2023-09-01 23:54:17 +0530

    Time: 0 ms (100%), Space: 7.9 MB (65.8%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,12 +1,32 @@
 class Solution {
 public:
-    vector<int> countBits(int n) {
-        vector<int> ans(n + 1);
+    // vector<int> countBits(int n) {
+    //     vector<int> ans(n + 1);
 
-    for (int i = 0; i <= n; i++) {
-        ans[i] = ans[i >> 1] + (i & 1);
-    }
+    // for (int i = 0; i <= n; i++) {
+    //     ans[i] = ans[i >> 1] + (i & 1);
+    // }
 
-    return ans;
+    // return ans;
+
+    // Used for the another approach to solve this problem 
+
+    vector<int> countBits(int n)
+    {
+        vector<int>ans(n+1);
+        int sum=0;
+        for(int i=0; i<=n; i++)
+        {
+            int num=i;
+            if(i%2!=0)
+            {
+                ans[i]=ans[i>>1]+1;
+            }
+            else{
+                ans[i] = ans[i>>1];
+            }
+        }
+        return ans;
     }
+    
 };
