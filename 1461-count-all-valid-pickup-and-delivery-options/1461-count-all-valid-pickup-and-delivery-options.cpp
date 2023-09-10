88893dbeed1d6ce64a0138c6f5083e601870c67b
Aryaman M Singha <91653939+Aryamnsls@users.noreply.github.com>
2023-09-11 02:14:49 +0530

    Time: 2 ms (57.5%), Space: 6 MB (44.3%) - LeetHub

--- 1461-count-all-valid-pickup-and-delivery-options/1461-count-all-valid-pickup-and-delivery-options.cpp
+++ 1461-count-all-valid-pickup-and-delivery-options/1461-count-all-valid-pickup-and-delivery-options.cpp
@@ -1,16 +1,13 @@
 class Solution {
-
-    const int MOD = 1e9+7;  // used for the find the min number  
+    // Using of the 10^9
+    int mod = 1e9+7;
 public:
     int countOrders(int n) {
-        // Using of Dp 
-
-        long long count = 1; // Create a variable 
-        for(int i=2; i<=n; i++)
-        {
-            count = ( count * (2*i-1) * i)% MOD;
+        
+        long long  count  = 1;
+        for(int i=2; i<=n; i++){
+            count  = (count*(2*i-1)*i)%mod;
         }
         return count;
-
     }
 };
