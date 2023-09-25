8d5fdb01bed0862d3d099c36eb6db6842e8f69c8
Aryaman M Singha <91653939+Aryamnsls@users.noreply.github.com>
2023-09-25 11:02:07 +0530

    Time: 6 ms (19.69%), Space: 7 MB (8.68%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,44 +1,30 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        //   // Firstly find the length
+        unordered_map<char, int>charCount;
 
-        //   // Using the two pointer approach 
-        //  char m = s.length(), n = t.length();
-        //  char cnt = 0;
-        //  for(int i = 0; i<m; i++)
-         
-        //      for(int j = n-2; j>=1; j--)
-             
-        //          if(s[i]==t[j])
-        //          // swap 
-        //     //    swap(s.first(), s.end() && t.first(), t.end());
-        //          cnt++;
-             
-         
-        //  return cnt;
+        // Traverse the char s 
+        for(char c: s){
+            charCount[c]++;
+        }
+        // Traverse the char t  
+        for(char c:t){
+           if(charCount.find(c)== charCount.end() || charCount[c]==0){
+               return c;
+           }
+           charCount[c]--;
 
-        // Using of ASCII Approach 
+        }
 
-         // Initialize two variables to store the ASCII values of characters in s and t
-    int charSumS = 0;
-    int charSumT = 0;
+        // All the characters are the same expect of one character 
 
-    // Calculate the sum of ASCII values of characters in s
-    for (char c : s) {
-        charSumS += c;
-    }
+        for(const auto&pair : charCount){
+            if(pair.second !=0)
+            {
+                return pair.first;
+            }
+        }
 
-    // Calculate the sum of ASCII values of characters in t
-    for (char c : t) {
-        charSumT += c;
-    }
-
-    // The extra character in t will have a higher sum than the sum of characters in s
-    // So, subtract the sum of characters in s from the sum of characters in t to find the ASCII value of the extra character
-    int diff = charSumT - charSumS;
-
-    // Convert the ASCII value back to a character and return it
-    return diff;
+       return ' ';
     }
 };
