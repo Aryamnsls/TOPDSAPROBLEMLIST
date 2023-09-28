a8ec3945ea18cba152805159ab158751f2ad10cd
Aryaman M Singha <91653939+Aryamnsls@users.noreply.github.com>
2023-09-28 13:31:41 +0530

    Time: 7 ms (66.47%), Space: 16.6 MB (25.63%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
@@ -1,25 +1,39 @@
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
-        // Create a vector 
-        vector<int>ans;
-        // Using the sort the array 
-        // sort(ans.begin(), ans.end());
-        // Traverse the for loop 
-        for(auto i: nums)
-        {
-            //  Check the value 
-            if(i%2==0){
-            ans.push_back(i);
-            }
-        }
+    //     // Create a vector 
+    //     vector<int>ans;
+    //     // Using the sort the array 
+    //     // sort(ans.begin(), ans.end());
+    //     // Traverse the for loop 
+    //     for(auto i: nums)
+    //     {
+    //         //  Check the value 
+    //         if(i%2==0){
+    //         ans.push_back(i);
+    //         }
+    //     }
     
-    for(auto j: nums)
+    // for(auto j: nums)
+    // {
+    //     if(j%2!=0){
+    //     ans.push_back(j);
+    //     }
+    // }
+    // return ans;
+
+    // Used another approach to slve this problem using 2-pointers
+    // Left index
+    int i=0, j = nums.size()-1;
+    while(i<j)
     {
-        if(j%2!=0){
-        ans.push_back(j);
+        if(nums[i]%2!=0){
+            swap(nums[i], nums[j]);
+            j--;
+        }else{
+            i++;
         }
     }
-    return ans;
+    return nums;
 }
 };
