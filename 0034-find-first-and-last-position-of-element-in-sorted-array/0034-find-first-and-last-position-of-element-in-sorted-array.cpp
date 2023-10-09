4f49e5024f5643aadd13b7a6ab896c69a998e418
Aryaman M Singha <91653939+Aryamnsls@users.noreply.github.com>
2023-10-09 12:32:50 +0530

    Time: 7 ms (51.15%), Space: 13.9 MB (37.79%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -2,71 +2,30 @@ class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         // Two-Pointer 
-        // int left = -1, right = -1;
-        // int n = nums.size();
-
-        // // Start from First Index
-        // for(int i = 0; i<n; i++)
-        // {
-        //     if(nums[i]==target){
-        //     left = i;
-        //     break;
-        // }
-        // }
-        // // End from right Index 
-        // for(int j = n-1; j>=0; j--)
-        // {
-        //     if(nums[j]==target){
-        //     right = j;
-        //     break;
-        // }
-        // }
-        // return{left, right};
-
-        // Used to solved by using map function
-         int left = findLeft(nums, target);
-    int right = findRight(nums, target);
-    return {left, right};
-}
-
-int findLeft(const std::vector<int>& nums, int target) {
-    int left = 0;
-    int right = nums.size() - 1;
-    int result = -1;
-
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (nums[mid] == target) {
-            result = mid;
-            right = mid - 1; // Look in the left half
-        } else if (nums[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
+        int left = -1, right = -1;
+        // Initialise the n size 
+        int n = nums.size();
+        // Start with the left index 
+        for(int i = 0; i<n; i++)
+        {
+            // Check that if we found the target element then we need to break 
+            if(nums[i]==target)
+            {
+                // Store it to the left index 
+                left = i;
+                break;
+            }
         }
-    }
-
-    return result;
-}
-
-int findRight(const std::vector<int>& nums, int target) {
-    int left = 0;
-    int right = nums.size() - 1;
-    int result = -1;
-
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (nums[mid] == target) {
-            result = mid;
-            left = mid + 1; // Look in the right half
-        } else if (nums[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
+        // Same goes for the right index as backwards attack  
+        for(int j = n-1; j>=0; j--)
+        {
+            // Check the value again if we found the target element 
+            if(nums[j]==target){
+            right = j;
+            break; 
+            }
         }
-    }
-
-    return result; 
-
+        // Atlast return the left and right 
+        return {left, right};
     }
 };
