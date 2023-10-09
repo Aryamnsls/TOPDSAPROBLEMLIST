90ddca5af46e07d39396b6815074d39da7d25e95
Aryaman M Singha <91653939+Aryamnsls@users.noreply.github.com>
2023-10-09 11:52:33 +0530

    Time: 11 ms (9.25%), Space: 14.1 MB (8.57%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -2,25 +2,71 @@ class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         // Two-Pointer 
-        int left = -1, right = -1;
-        int n = nums.size();
+        // int left = -1, right = -1;
+        // int n = nums.size();
 
-        // Start from First Index
-        for(int i = 0; i<n; i++)
-        {
-            if(nums[i]==target){
-            left = i;
-            break;
+        // // Start from First Index
+        // for(int i = 0; i<n; i++)
+        // {
+        //     if(nums[i]==target){
+        //     left = i;
+        //     break;
+        // }
+        // }
+        // // End from right Index 
+        // for(int j = n-1; j>=0; j--)
+        // {
+        //     if(nums[j]==target){
+        //     right = j;
+        //     break;
+        // }
+        // }
+        // return{left, right};
+
+        // Used to solved by using map function
+         int left = findLeft(nums, target);
+    int right = findRight(nums, target);
+    return {left, right};
+}
+
+int findLeft(const std::vector<int>& nums, int target) {
+    int left = 0;
+    int right = nums.size() - 1;
+    int result = -1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target) {
+            result = mid;
+            right = mid - 1; // Look in the left half
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
         }
+    }
+
+    return result;
+}
+
+int findRight(const std::vector<int>& nums, int target) {
+    int left = 0;
+    int right = nums.size() - 1;
+    int result = -1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target) {
+            result = mid;
+            left = mid + 1; // Look in the right half
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
         }
-        // End from right Index 
-        for(int j = n-1; j>=0; j--)
-        {
-            if(nums[j]==target){
-            right = j;
-            break;
-        }
-        }
-        return{left, right};
+    }
+
+    return result; 
+
     }
 };
